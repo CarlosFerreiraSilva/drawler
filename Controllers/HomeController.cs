12b9a3442356aac24e132f28bf745b7faa0e1c41
Nitierk <115711742+Mario-Cesar1@users.noreply.github.com>
2023-04-10 18:05:31 -0300

    a

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using CoderCarrer.DAL;
+using CoderCarrer.Domain;
+using CoderCarrer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace CoderCarrer.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IExtratorVaga vagasDao = new TrabalhaBrasilDAO();
+            IExtratorVaga vagasfake = new VagasFake();
+            List<Vaga> vagas = vagasDao.getVagas();
+            List<Vaga> vagasfaike = vagasfake.getVagas();
+
+            List<Vaga> vaga = new List<Vaga>();
+            vaga.AddRange(vagas);
+
+
+            return View(vaga);
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public async Task<IActionResult> Pesquisa()
+        {
+            IExtratorVaga vagasDao = new TrabalhaBrasilDAO();
+            IExtratorVaga vagasfake = new VagasFake();
+            List<Vaga> vagas = vagasDao.getVagas();
+            List<Vaga> vagasfaike = vagasfake.getVagas();
+
+            List<Vaga> vaga = new List<Vaga>();
+            vaga.AddRange(vagas);
+
+
+            return View(vaga);
+
+        }
+
+       
+    }
+}
---
+++ DAL/VagasFake.cs
@@ -0,0 +1,43 @@
+﻿using CoderCarrer.Domain;
+using CoderCarrer.Models;
+
+namespace CoderCarrer.DAL
+{
+    public class VagasFake : IExtratorVaga
+    {
+        public List<Vaga> getVagas()
+        {
+            List<Vaga> vagas = new List<Vaga>();
+            List<Vaga> _listafake;
+            _listafake = new List<Vaga>();
+
+            Vaga vaga1 = new Vaga();
+            vaga1.titulo = "Programador Junior";
+            vaga1.empresa = "HWUAI";
+            vaga1.descricao_vaga = "Produção";
+            _listafake.Add(vaga1);
+
+            Vaga vaga2 = new Vaga();
+            vaga2.titulo = "Psicologa";
+            vaga2.empresa = "CAPS";
+            vaga2.descricao_vaga = "Preferência louca";
+            _listafake.Add(vaga2);
+
+            Vaga vaga3 = new Vaga();
+            vaga3.titulo = "Jogador de LOL";
+            vaga3.empresa = "Casa de Maia";
+            vaga3.descricao_vaga = "Jogador de lol main sup";
+            _listafake.Add(vaga3);
+
+            Vaga vaga4 = new Vaga();
+            vaga4.titulo = "Professor";
+            vaga4.empresa = "Senai";
+            vaga4.descricao_vaga = "Professor dessa porra";
+            _listafake.Add(vaga4);
+
+            
+
+            return _listafake;
+        }
+    }
+}
